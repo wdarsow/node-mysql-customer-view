73f006543eb281ab0d44a81de45841ac3b01fe67
wdarsow <wade.darsow@gmail.com>
2018-05-08 16:17:45 -0500

    final changes

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,13 +1,17 @@
+
+// use the mysql and inquirer NPM packages and assign them to variables
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 
+// response variables for use in functions
 let response1;
 let response2;
 
+// variables used to store input values from the user through inquirer
 let idNum2;
 let numUnits2;
 
-
+// define a connection to my mysql database and assign the required parameters to a const variable
 const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -16,12 +20,13 @@ const connection = mysql.createConnection({
     database: "bamazon"
 });
 
+// instantiate a connection to my mysql database
 connection.connect(function(error) {
     if(error) throw error;
     console.log("You have successfully connected to the bamazon MySQL database");
-    //connection.end();
 });
 
+// prompt is a function that asks the user 2 questions leveraging the inquirer package
 let prompt = function() {
     inquirer.prompt([
         {
@@ -36,19 +41,15 @@ let prompt = function() {
         let idNum = answers.IDtobuy - 1;
         let numUnits = answers.unitsOfProduct;
 
-        console.log(numUnits);
-        console.log(response1[idNum].stock_quantity)
-
         if(numUnits > response1[idNum].stock_quantity) {
-            console.log("Insufficient quantity!")
+            console.log("Insufficient quantity!");
         } else {
             console.log("The total cost of your purchase is: $", (response1[idNum].price * numUnits));
-            //connection.end();
             let updateDatabase = function(error, responses) {
                 if(error) {
                     throw error;
                 }
-                console.log("updating product");
+
                 connection.query("UPDATE products SET ? WHERE ?", 
                 [
                     {
@@ -58,35 +59,15 @@ let prompt = function() {
                         item_id: idNum + 1
                     }
                 ], 
-                
-                //function (error, response) {
-                  // console.log("Item ID: " + response[i].item_id + " | Product Name: " + response[i].product_name + " | Price: " + response[i].price);
-                //console.log(response);  
                 )};
-            //let customer2 = function() {
-             //   connection.query("SELECT * FROM products", [0], function (error, response) {
-                        //console.log(response);
-               //     }
-                    //response1 = response;
-                    //prompt();
-                    
-               // );    
-               console.log(response1[idNum].stock_quantity);
-               console.log(numUnits);
-               console.log(idNum);
-           // };
+            
             updateDatabase();
         };
             connection.end();
-            //response2 = response1;   
-           // idNum2 = idNum;
-            //numUnits2 = numUnits;
-            //updateDatabase();
         });
     };
-   //});
-//}
 
+// this function updates the stock quanitity in the mysql database after a purchase is made
 let updateDatabase = function(error) {
     if(error) {
         throw error;
@@ -101,12 +82,10 @@ let updateDatabase = function(error) {
             item_id: idNum2
         }
     ], 
+)};
 
-    //function (error, response) {
-      // console.log("Item ID: " + response[i].item_id + " | Product Name: " + response[i].product_name + " | Price: " + response[i].price);
-    //console.log(response);  
-    )};
 
+// this customer function displays all of the items and their associated quantities from the mysql database in the console
 let customer = function() {
     connection.query("SELECT * FROM products", [0], function (error, response) {
         for (let i = 0; i < response.length; i++) {
@@ -117,7 +96,5 @@ let customer = function() {
     });    
 };
 
+// this calls the customer function
 customer();
-
-//prompt();
-//inquirer.prompt()
